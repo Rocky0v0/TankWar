5feddd42ad7c70176919cbfdd8926a9a073e0498
Rockyovo <k19121651537@outlook.com>
2022-05-31 12:07:21 +1000

    构建类结构

---
+++ TankWar_01/MoveThing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankWar_01
+{
+    enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+    }
+    internal class MoveThing:GameObject
+    {
+        public int Speed { get; set; }
+
+        public Direction Dir { get; set; }
+
+        public Bitmap BitmapUp { get; set; }
+        public Bitmap BitmapDown { get; set; }
+        public Bitmap BitmapRight { get; set; }
+        public Bitmap BitmapLeft { get; set; }
+        
+        
+    }
+}
