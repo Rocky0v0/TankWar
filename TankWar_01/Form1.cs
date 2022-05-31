266f9d0e8765eb53605e4a589605879186abdfb0
Rockyovo <k19121651537@outlook.com>
2022-05-31 11:26:38 +1000

    添加项目文件。

---
+++ TankWar_01/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TankWar_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            Thread t = new Thread(new ThreadStart(GameMainThread));
+            t.Start();
+
+        }
+
+        public static void GameMainThread()
+        {
+
+        }
+    }
+}
